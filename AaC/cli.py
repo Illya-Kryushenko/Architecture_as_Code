79ae71796102ec06e1ababc17fa1bfcd8c3e8d96
Illya-Kryushenko <czkicker@gmail.com>
2026-04-23 13:04:44 +0200

    Entrance point

---
+++ AaC/cli.py
@@ -0,0 +1,16 @@
+import argparse
+from .model import load_model
+from .validator import check_model_against_terraform_state
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", required=True)
+    parser.add_argument("--state", required=True)
+    args = parser.parse_args()
+
+    model = load_model(args.model)
+    success = check_model_against_terraform_state(model, args.state)
+    exit(0 if success else 1)
+
+if __name__ == "__main__":
+    main()
